448e912ebc6a1991fb2182ce0fcd363b94e40d7a
Haroon Ur Rasheed <reach.haroonrasheed@gmail.com>
2025-08-01 11:33:17 +0500

    initial commit

---
+++ driver.py
@@ -0,0 +1,1 @@
+print("This is line 1 in main branch on github")
--- version1.py
+++
@@ -1,6 +0,0 @@
-print("This is the version1 of my Github project to test the basis like creating other branches and managing changes.")
-print("This is some other file here")
-
-print("This statement is added at version2 branch!")
-
-print("A new line added in version2 branch")
