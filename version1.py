9d30dcd40eb5b2231dc487d94779d00d8dc520a0
Haroon Ur Rasheed <reach.haroonrasheed@gmail.com>
2025-08-01 11:07:20 +0500

    A new line on version1.py in version2branch

--- version1.py
+++ version1.py
@@ -2,3 +2,5 @@ print("This is the version1 of my Github project to test the basis like creating
 print("This is some other file here")
 
 print("This statement is added at version2 branch!")
+
+print("A new line added in version2 branch")
